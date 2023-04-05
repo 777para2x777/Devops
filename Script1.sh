a99b4bc739512f183da4244a445f10734abead27
paradox <m.nosrati1987>
2023-04-05 11:12:21 +0330

    New Change

--- Script1.sh
+++ Script1.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-echo "hello $USER"
+echo "helo $USER"
 echo  "heddllo $1"
 echo "Nddothing"
 read -p "please Enter your age" var1
-echo "your age is : " $var1
+echo "your age is : " $var0
